7fe6f09e6abf46a9024927d38b91193645e4e872
madhumitha13 <36561411+madhumitha13@users.noreply.github.com>
2018-06-19 12:10:15 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class alpha
+{
+public static void main(String ags[])
+{
+char ch="*";
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+{
+System.out.print("it is an Alphabet");
+}
+else
+{
+System.out.print("Not");
+}
+}
+}
